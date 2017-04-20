e5fa2c8b2d5b7a7b8f36ed26e72ba21b619f7d88
SemiWarm <semiwarm@163.com>
2017-04-20 19:36:15 +0800

    为类目Banner图片添加了遮罩

--- app/src/main/java/app/semiwarm/cn/fragment/SortItemFragment.java
+++ app/src/main/java/app/semiwarm/cn/fragment/SortItemFragment.java
@@ -3,7 +3,7 @@ package app.semiwarm.cn.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +17,13 @@ import com.bumptech.glide.Glide;
 
 import app.semiwarm.cn.R;
 import app.semiwarm.cn.entity.Category;
+import butterknife.BindColor;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 /**
- * 子类
+ * 类目页详情
  * A simple {@link Fragment} subclass.
  */
 public class SortItemFragment extends Fragment {
@@ -31,10 +32,10 @@ public class SortItemFragment extends Fragment {
     LinearLayout mContainerLinearLayout;
     @BindView(R.id.rl_banner_container)
     RelativeLayout mBannerContainerRelativeLayout;
-    @BindView(R.id.tv_category_desc)
-    TextView mCategoryDescTextView;
     @BindView(R.id.tv_category_title)
     TextView mCategoryTitleTextView;
+    @BindColor(R.color.white)
+    int mColor;
 
     private int mContainerWidth;
 
@@ -57,7 +58,6 @@ public class SortItemFragment extends Fragment {
                 if (category != null) {
                     // 根据容器宽度计算图片宽度
                     int categoryBannerImageWidth = mContainerWidth * 14 / 15;
-                    Log.i("width:", "" + categoryBannerImageWidth);
                     // 根据图片宽度计算图片高度
                     int categoryBannerImageHeight = categoryBannerImageWidth * 5 / 16;
                     // 初始化图片
@@ -69,13 +69,31 @@ public class SortItemFragment extends Fragment {
                             .bitmapTransform(new RoundedCornersTransformation(getContext(), 10, 0))
                             .into(categoryBannerImageView);
                     // 设置布局
-                    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(categoryBannerImageWidth, categoryBannerImageHeight);
-                    categoryBannerImageView.setLayoutParams(layoutParams);
+                    RelativeLayout.LayoutParams categoryBannerLayoutParams = new RelativeLayout.LayoutParams(categoryBannerImageWidth, categoryBannerImageHeight);
+                    categoryBannerImageView.setLayoutParams(categoryBannerLayoutParams);
                     // 添加到布局
                     mBannerContainerRelativeLayout.addView(categoryBannerImageView);
 
-                    // 加载描述
-                    mCategoryDescTextView.setText(category.getCategoryDesc());
+                    // 初始化遮罩图片
+                    ImageView coverBannerImageView = new ImageView(getContext());
+                    coverBannerImageView.setBackground(getContext().getDrawable(R.drawable.bg_cover_image));
+                    // 设置遮罩布局
+                    coverBannerImageView.setLayoutParams(categoryBannerLayoutParams);
+                    // 添加到布局
+                    mBannerContainerRelativeLayout.addView(coverBannerImageView);
+
+                    // 初始化描述TextView
+                    TextView categoryDescTextView = new TextView(getContext());
+                    categoryDescTextView.setText(category.getCategoryDesc());
+                    categoryDescTextView.setTextColor(mColor);
+                    categoryDescTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP,15);
+
+                    // 设置TextView布局参数
+                    RelativeLayout.LayoutParams categoryDescLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                    categoryDescLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+                    categoryDescTextView.setLayoutParams(categoryDescLayoutParams);
+                    // 添加到布局
+                    mBannerContainerRelativeLayout.addView(categoryDescTextView);
                     // 加载标题
                     mCategoryTitleTextView.setText(category.getCategoryTitle());
                 }
